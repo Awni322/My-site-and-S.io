b7d7204fb59b9a697c4cacc42473c9bd6df5ad3c
Awni322 <awnirr82@gmail.com>
2026-07-27 03:49:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+const WORKER_URL = "https://my-password-check.minecraftpesok.workers.dev/";
+
+
 async function login(){
 
     let password = document.getElementById("password").value;
     let message = document.getElementById("message");
 
+
     message.innerHTML = "Проверка...";
 
+
     let response = await fetch(
-        "https://my-password-check.minecraftpesok.workers.dev/",
+        WORKER_URL,
         {
             method:"POST",
             headers:{
                 "Content-Type":"application/json"
             },
-            body: JSON.stringify({
-                password: password
+            body:JSON.stringify({
+                password:password
             })
         }
     );
@@ -24,13 +29,117 @@ async function login(){
         message.innerHTML = "✅ Пароль верный";
         message.style.color = "green";
 
+
         document.getElementById("login").style.display = "none";
         document.getElementById("content").style.display = "block";
 
+
+        loadNotes();
+
+
     } else {
+
 
         message.innerHTML = "❌ Неверный пароль";
         message.style.color = "red";
 
+
     }
+
+}
+
+
+
+
+
+async function saveNote(){
+
+
+    let title = document.getElementById("title").value;
+    let text = document.getElementById("text").value;
+
+
+    if(!title || !text){
+        alert("Заполни название и текст");
+        return;
+    }
+
+
+
+    let response = await fetch(
+        WORKER_URL,
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify({
+
+                action:"save",
+                title:title,
+                content:text
+
+            })
+
+        }
+    );
+
+
+
+    if(response.ok){
+
+        document.getElementById("title").value="";
+        document.getElementById("text").value="";
+
+
+        loadNotes();
+
+    }
+
+}
+
+
+
+
+
+async function loadNotes(){
+
+
+    let response = await fetch(
+        WORKER_URL
+    );
+
+
+    let notes = await response.json();
+
+
+    let output = "";
+
+
+    notes.forEach(note => {
+
+
+        output += `
+
+        <div>
+
+            <h3>📌 ${note.title}</h3>
+
+            <p>${note.content}</p>
+
+            <hr>
+
+        </div>
+
+        `;
+
+
+    });
+
+
+
+    document.getElementById("notes").innerHTML = output;
+
+
 }
